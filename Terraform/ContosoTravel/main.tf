0df084d13660afb8f49ff5a6869d43130a8e1305
Andy Wahrenberger <andywahr@microsoft.com>
2019-02-12 12:55:11 -0600

    changed to use ambiant account

--- Terraform/ContosoTravel/main.tf
+++ Terraform/ContosoTravel/main.tf
@@ -190,7 +190,7 @@ resource "azurerm_key_vault_access_policy" "deployKeyVaultPolicy" {
   key_vault_id = "${azurerm_key_vault.keyVault.id}"
 
   tenant_id = "${data.azurerm_client_config.current.tenant_id}"
-  object_id = "${var.servicePrincipalId}"
+  object_id = "${data.azurerm_client_config.current.service_principal_application_id}"
 
   secret_permissions = [
     "set",
