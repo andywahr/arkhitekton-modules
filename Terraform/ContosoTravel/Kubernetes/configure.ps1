d9610a6c4cf167bf791ca16b2ca3fbe7defaae61
Andy Wahrenberger <andywahr@microsoft.com>
2019-03-12 16:32:09 -0500

    support kuberentes for services

--- Terraform/ContosoTravel/Kubernetes/configure.ps1
+++ Terraform/ContosoTravel/Kubernetes/configure.ps1
@@ -23,14 +23,16 @@ $id = az identity list --query [].id --output tsv  --resource-group $rg --subscr
 $clientId = az identity list --query [].clientId --output tsv  --resource-group $rg --subscription $sub
 $appInsightsKey = az resource show --resource-group $rg --subscription $sub --resource-type Microsoft.Insights/components --name ($namePrefix +"appInsightContosoTravel") --query "properties.InstrumentationKey"
 $dnsZone = (az aks show --resource-group $rg --subscription $sub --name ("aks-ContosoTravel-" + $namePrefix) --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName).Replace('"', '')
+$serviceConnStr = (az keyvault secret show --resource-group $rg --subscription $sub --vault-name ("kv" + $namePrefix) --name connstr --query value).Replace('"', '')
 
 function replaceInFiles([string]$fileName) {
   $contents = get-content $fileName | out-string
   $contents = $contents.Replace('$CLIENTID$', $clientId)
-  $contents = $contents.Replace('$ID$', $id)
   $contents = $contents.Replace('$APPINSIGHTSKEY$', $appInsightsKey)
   $contents = $contents.Replace('$NAMEPREFIX$', $namePrefix)
   $contents = $contents.Replace('$DNSZONE$', $dnsZone)
+  $contents = $contents.Replace('$SERVICECONNECTIONSTRING$', $serviceConnStr)
+  $contents = $contents.Replace('$ID$', $id)
   set-content -Path $fileName -Value $contents -Force
 }
 
---
+++ Terraform/ContosoTravel/backend/kubernetes/main.tf
@@ -0,0 +1,90 @@
+data "azurerm_client_config" "current" {}
+
+variable "namePrefix" {
+  type = "string"
+}
+
+variable "location" {
+  type = "string"
+}
+
+variable "resourceGroupName" {
+  type = "string"
+}
+
+variable "keyVaultUrl" {
+  type = "string"
+}
+
+variable "keyVaultAccountName" {
+  type = "string"
+}
+
+variable "keyVaultId" {
+  type = "string"
+}
+
+variable "appInsightsKey" {
+  type = "string"
+}
+
+variable "storageAccountId" {
+  type = "string"
+}
+
+variable "logAnalyticsId" {
+  type = "string"
+}
+
+variable "logAnalyticsName" {
+  type = "string"
+}
+
+variable "vnetName" {
+  type = "string"
+}
+
+variable "vnetId" {
+  type = "string"
+}
+
+variable "servicePrincipalObjectId" {
+  type = "string"
+}
+
+variable "servicePrincipalClientId" {
+  type = "string"
+}
+
+variable "servicePrincipalSecretName" {
+  type = "string"
+}
+
+
+module "aksInstall" {
+  source = "../../Kubernetes"
+  namePrefix = "${var.namePrefix}"
+  location = "${var.location}"
+  resourceGroupName = "${var.resourceGroupName}"
+  keyVaultUrl = "${var.keyVaultUrl}"
+  keyVaultAccountName = "${var.keyVaultAccountName}"
+  keyVaultId = "${var.keyVaultId}"
+  appInsightsKey = "${var.appInsightsKey}"
+  storageAccountId = "${var.storageAccountId}" 
+  logAnalyticsId = "${var.logAnalyticsId}"
+  logAnalyticsName = "${var.logAnalyticsName}"
+  vnetName = "${var.vnetName}"
+  vnetId = "${var.vnetId}"
+  servicePrincipalClientId = "${var.servicePrincipalClientId}"
+  servicePrincipalObjectId = "${var.servicePrincipalObjectId}"
+  servicePrincipalSecretName = "${var.servicePrincipalSecretName}"
+}
+
+# Create Static Public IP Address to be used by Nginx Ingress
+resource "azurerm_public_ip" "nginx_ingress" {
+  name                = "aks-ContosoTravel-${var.namePrefix}-nginx-ingress-pip"
+  location            = "${var.location}"
+  resource_group_name = "${var.resourceGroupName}"
+  allocation_method   = "Static"
+  domain_name_label   = "www-aks-contosotravel-${lower(var.namePrefix)}"
+}
