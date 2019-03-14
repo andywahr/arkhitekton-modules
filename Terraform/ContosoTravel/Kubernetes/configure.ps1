10ccac22a00b0d3dfaba87610ae767b5db06bf48
Andy Wahrenberger <andywahr@microsoft.com>
2019-03-14 14:53:50 -0500

    fixed serviceConnStr get

--- Terraform/ContosoTravel/Kubernetes/configure.ps1
+++ Terraform/ContosoTravel/Kubernetes/configure.ps1
@@ -18,12 +18,16 @@ param
 "Get Credentials"
 az aks get-credentials --resource-group $rg --subscription $sub --name ("aks-ContosoTravel-" + $namePrefix) --overwrite-existing
 
-"Get Info From Resource Group"
+"Get Info From Resource Group - identity"
 $id = az identity list --query [].id --output tsv  --resource-group $rg --subscription $sub
+"Get Info From Resource Group - clientId"
 $clientId = az identity list --query [].clientId --output tsv  --resource-group $rg --subscription $sub
+"Get Info From Resource Group - appInsightsKey"
 $appInsightsKey = az resource show --resource-group $rg --subscription $sub --resource-type Microsoft.Insights/components --name ($namePrefix +"appInsightContosoTravel") --query "properties.InstrumentationKey"
+"Get Info From Resource Group - dnsZone"
 $dnsZone = (az aks show --resource-group $rg --subscription $sub --name ("aks-ContosoTravel-" + $namePrefix) --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName).Replace('"', '')
-$serviceConnStr = (az keyvault secret show --resource-group $rg --subscription $sub --vault-name ("kv" + $namePrefix) --name connstr --query value).Replace('"', '')
+"Get Info serviceConnStr Resource Group - dnsZone"
+$serviceConnStr = (az keyvault secret show --subscription $sub --vault-name "kv$namePrefix" --name "ContosoTravel--ServiceConnectionString" --query value).Replace('"', '')
 
 function replaceInFiles([string]$fileName) {
   $contents = get-content $fileName | out-string
