8a7f473349c34affc21f9e6850cb5ac3ee155c99
Andy Wahrenberger <andywahr@microsoft.com>
2019-03-23 22:30:02 -0400

    addng MySQL

---
+++ Terraform/ContosoTravel/data/mysql/main.tf
@@ -0,0 +1,189 @@
+variable "namePrefix" {
+  type = "string"
+}
+
+variable "location" {
+  type = "string"
+}
+
+variable "resourceGroupName" {
+  type = "string"
+}
+
+variable "aciVnetId" {
+  type = "string"
+}
+
+variable "vnetId" {
+  type = "string"
+}
+variable "storageAccountId" {
+  type = "string"
+}
+
+variable "logAnalyticsId" {
+  type = "string"
+}
+
+variable "logAnalyticsName" {
+  type = "string"
+}
+
+variable "keyVaultId" {
+  type = "string"
+}
+
+resource "random_string" "dataAdministratorLogin" {
+  length = 16
+  special = false
+}
+
+resource "random_string" "dataAdministratorLoginPassword" {
+  length = 32
+  special = true
+}
+
+resource "random_string" "dataAccountUserName" {
+  length = 16
+  special = false
+}
+
+resource "random_string" "dataAccountPassword" {
+  length = 32
+  special = true
+}
+
+resource "azurerm_mysql_server" "mysqlServer" {
+  name                         = "contosotravel-sqlserver-${var.namePrefix}"
+  resource_group_name          = "${var.resourceGroupName}"
+  location                     = "${var.location}"
+  version                      = "5.7"
+  ssl_enforcement              = "Enabled"  
+  administrator_login          = "${random_string.dataAdministratorLogin.result}"
+  administrator_login_password = "${random_string.dataAdministratorLoginPassword.result}"
+
+
+    sku {
+    name     = "B_Gen5_2"
+    capacity = 2
+    tier     = "Basic"
+    family   = "Gen5"
+  }
+
+  storage_profile {
+    storage_mb            = 5120
+    backup_retention_days = 7
+    geo_redundant_backup  = "Disabled"
+  }
+}
+
+resource "azurerm_mysql_database" "mysqlServerDatabase" {
+  name                = "ContosoTravel"
+  resource_group_name = "${var.resourceGroupName}"
+  location            = "${var.location}"
+  server_name         = "${azurerm_mysql_server.mysqlServer.name}"
+
+  tags {
+    environment = "production"
+  }
+}
+
+# Add back in when ACI VNet work
+#
+#resource "azurerm_mysql_virtual_network_rule" "sqlAppVnetrule" {
+#  name                = "sql-vnet-rule-app"
+#  resource_group_name = "${var.resourceGroupName}"
+#  server_name         = "${azurerm_mysql_server.mysqlServer.name}"
+#  subnet_id           = "${var.vnetId}"
+#}
+#
+#resource "azurerm_mysql_virtual_network_rule" "sqlAciVnetrule" {
+#  name                = "sql-vnet-rule-aci"
+#  resource_group_name = "${var.resourceGroupName}"
+#  server_name         = "${azurerm_mysql_server.mysqlServer.name}"
+#  subnet_id           = "${var.aciVnetId}"
+#}
+resource "azurerm_mysql_firewall_rule" "sqlAllAzureServices" {
+  name                = "sqlAllAzureServicesRule"
+  resource_group_name = "${var.resourceGroupName}"
+  server_name         = "${azurerm_mysql_server.mysqlServer.name}"
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+resource "azurerm_mysql_firewall_rule" "sqlAllServices" {
+  name                = "sqlAllAzureServicesRule"
+  resource_group_name = "${var.resourceGroupName}"
+  server_name         = "${azurerm_mysql_server.mysqlServer.name}"
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
+}
+
+resource "azurerm_monitor_diagnostic_setting" "dataDiag" {
+  name               = "${var.namePrefix}-dataDiag"
+  target_resource_id = "${azurerm_mysql_server.mysqlServer.id}"
+  storage_account_id = "${var.storageAccountId}"
+  log_analytics_workspace_id = "${var.logAnalyticsId}"
+  
+  metric {
+    category = "AllMetrics"
+
+    retention_policy {
+      enabled = false
+    }
+  }
+}
+
+#resource "azurerm_log_analytics_solution" "sqlInsights" {
+#  solution_name         = "SQLInsights"
+#  location            = "East US"
+#  resource_group_name = "${var.resourceGroupName}"
+#  workspace_resource_id = "${var.logAnalyticsId}"
+#  workspace_name        = "${var.logAnalyticsName}"
+#
+#  plan {
+#    publisher = "Microsoft"
+#    product   = "OMSGallery/AzureSQLAnalytics"
+#  }
+#}
+
+resource "azurerm_key_vault_secret" "dataType" {
+  name     = "ContosoTravel--DataType"
+  value    = "MySQL"
+  key_vault_id = "${var.keyVaultId}"
+}
+
+resource "azurerm_key_vault_secret" "dataAccountName" {
+  name     = "ContosoTravel--DataAccountName"
+  value    = "${azurerm_mysql_server.mysqlServer.name}"
+  key_vault_id = "${var.keyVaultId}"
+}
+
+resource "azurerm_key_vault_secret" "databaseName" {
+  name     = "ContosoTravel--DatabaseName"
+  value    = "ContosoTravel"
+  key_vault_id = "${var.keyVaultId}"
+}
+
+resource "azurerm_key_vault_secret" "dataAdministratorLogin" {
+  name     = "ContosoTravel--DataAdministratorLogin"
+  value    = "${random_string.dataAdministratorLogin.result}"
+  key_vault_id = "${var.keyVaultId}"
+}
+
+resource "azurerm_key_vault_secret" "dataAdministratorLoginPassword" {
+  name     = "ContosoTravel--DataAdministratorLoginPassword"
+  value    = "${random_string.dataAdministratorLoginPassword.result}"
+  key_vault_id = "${var.keyVaultId}"
+}
+
+resource "azurerm_key_vault_secret" "dataAccountUserName" {
+  name     = "ContosoTravel--DataAccountUserName"
+  value    = "${random_string.dataAccountUserName.result}"
+  key_vault_id = "${var.keyVaultId}"
+}
+
+resource "azurerm_key_vault_secret" "dataAccountPassword" {
+  name     = "ContosoTravel--DataAccountPassword"
+  value    = "${random_string.dataAccountPassword.result}"
+  key_vault_id = "${var.keyVaultId}"
+}
