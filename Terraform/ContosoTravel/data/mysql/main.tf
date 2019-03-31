95a22dfc4fa1f5fc00a799567e0856ac050a5345
Andy Wahrenberger <andywahr@microsoft.com>
2019-03-31 14:40:35 -0500

    math is hard, 16-5 is 11 not 13

--- Terraform/ContosoTravel/data/mysql/main.tf
+++ Terraform/ContosoTravel/data/mysql/main.tf
@@ -34,7 +34,7 @@ variable "keyVaultId" {
 }
 
 resource "random_string" "dataAdministratorLogin" {
-  length = 13
+  length = 11
   special = false
 }
 
@@ -44,7 +44,7 @@ resource "random_string" "dataAdministratorLoginPassword" {
 }
 
 resource "random_string" "dataAccountUserName" {
-  length = 13
+  length = 11
   special = false
 }
 
